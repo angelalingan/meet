3030090a1ea32bf119b8f2a007444918fe7e0f88
Angela Lingan Tan <angelalingan@gmail.com>
2023-02-03 13:08:01 -0600

    added NumberofEvents

---
+++ src/tests/NumberOfEvents.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+    let NumberOfEventsWrapper;
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(
+            <NumberOfEvents updateNumberOfEvents={() => { }} />
+        );
+    });
+
+    test('renders the component', () => {
+        expect(NumberOfEventsWrapper).toBeDefined();
+    });
+
+    test('default number of event value is 32', () => {
+        expect(NumberOfEventsWrapper.find('input.num').prop('type')).toBe('number');
+        expect(NumberOfEventsWrapper.state('num')).toBe(32);
+    });
+
+    test('user can change value for number of events', () => {
+        expect(NumberOfEventsWrapper.state('num')).toBe(32);
+
+        NumberOfEventsWrapper.find('input.num').simulate('change', {
+            target: { value: 24 }
+        });
+        expect(NumberOfEventsWrapper.state('num')).toBe(24);
+    });
+
+    test('rendered number of events equals the value the user has chosen', () => {
+        const NumberOfEventsWrapperWithProp = shallow(
+            <NumberOfEvents num={10} updateNumberOfEvents={() => { }} />
+        );
+
+        expect(NumberOfEventsWrapperWithProp.state('num')).toBe(10);
+    });
+})
